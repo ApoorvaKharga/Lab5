af8c045cde3a8c748777d7c5ff92b22c4fff4115
Apoorva Kharga <apoorvakharga@gmail.com>
2026-02-20 12:37:33 +0530

    initial

--- src/LQ1.java
+++ src/LQ1.java
@@ -1,1 +1,24 @@
+import Marketing.sales;
+import java.util.Scanner;
 
+public class LQ1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the employee id and employee name: ");
+        int id = sc.nextInt();
+        String name = sc.next();
+
+        System.out.print("Enter the basic salary: ");
+        double basic = sc.nextDouble();
+
+        sales s = new sales(id, name, basic);
+
+        double totalEarning = s.earnings();
+
+        System.out.println("The emp id of the employee is " + s.getEmpid());
+        System.out.println("The total earning is " + totalEarning);
+    }
+}
--- src/LQ2.java
+++ src/LQ2.java
@@ -1,53 +1,56 @@
 import java.util.Scanner;
 
 public class LQ2 {
-
-    static boolean isVowel(char ch) {
-        ch = Character.toLowerCase(ch);
-        return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
-    }
-
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a String: ");
         String str = sc.nextLine();
+        String changedCase = str.toUpperCase();
+        System.out.println("The string after changing the case is " + changedCase);
 
-        System.out.println("The string after changing the case is " + str.toUpperCase());
-        System.out.println("The string after reversing is " +
-                new StringBuilder(str).reverse());
 
-        System.out.print("Enter the second string for comparision: ");
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println("The string after reversing is " + reversed);
+
+
+        System.out.print("Enter the second string for comparison: ");
         String str2 = sc.nextLine();
-        System.out.println("The difference between ASCII value is " +
-                Math.abs(str.compareTo(str2)));
+        int diff = Math.abs(str.compareTo(str2));
+        System.out.println("The difference between ASCII value is " + diff);
+
 
         System.out.print("Enter the string to be inserted into first string: ");
         String insert = sc.nextLine();
-        System.out.println("The string after insertion is: " + str + " " + insert);
+        String combined = str + " " + insert;
+        System.out.println("The string after insertion is: " + combined);
 
         System.out.print("Enter a String: ");
-        String s = sc.nextLine();
-        System.out.println("Uppercase: " + s.toUpperCase());
-        System.out.println("Lowercase: " + s.toLowerCase());
+        String caseStr = sc.nextLine();
+        System.out.println("Uppercase: " + caseStr.toUpperCase());
+        System.out.println("Lowercase: " + caseStr.toLowerCase());
+
 
         System.out.print("Enter a String: ");
-        String s3 = sc.nextLine();
+        String charStr = sc.nextLine();
         System.out.print("Enter a character: ");
         char ch = sc.next().charAt(0);
 
-        int pos = s3.indexOf(ch);
-        if (pos != -1)
-            System.out.println("Position of entered character: " + pos);
+        int position = charStr.indexOf(ch);
+        if (position != -1)
+            System.out.println("Position of entered character: " + position);
         else
             System.out.println("Entered character is not present");
 
-        sc.nextLine();
+        sc.nextLine(); // clear buffer
+
 
         System.out.print("Enter a String: ");
         String pal = sc.nextLine();
-        String rev = new StringBuilder(pal).reverse().toString();
-        if (pal.equalsIgnoreCase(rev))
+        String revPal = new StringBuilder(pal).reverse().toString();
+
+        if (pal.equals(revPal))
             System.out.println("Entered string is palindrome");
         else
             System.out.println("Entered string is not a palindrome");
@@ -55,19 +58,22 @@ public class LQ2 {
         System.out.print("Enter a String: ");
         String sentence = sc.nextLine();
 
-        int vowels = 0, consonants = 0;
-        String[] words = sentence.split("\\s+");
+        String[] words = sentence.trim().split("\\s+");
+        int wordCount = words.length;
 
-        for (char c : sentence.toCharArray()) {
+        int vowels = 0, consonants = 0;
+
+        for (char c : sentence.toLowerCase().toCharArray()) {
             if (Character.isLetter(c)) {
-                if (isVowel(c)) vowels++;
-                else consonants++;
+                if ("aeiou".indexOf(c) != -1)
+                    vowels++;
+                else
+                    consonants++;
             }
         }
 
-        System.out.println("No. of words: " + words.length);
+        System.out.println("No. of words: " + wordCount);
         System.out.println("No. of vowels: " + vowels);
         System.out.println("No. of consonants: " + consonants);
     }
 }
-
